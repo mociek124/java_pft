b33c9ea756d7dbc83bc6b409722f8cccb5e8d81f
mociek124 <mociek124@gmail.com>
2017-03-22 18:24:10 +0100

    program Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,22 @@
 package ru.stqa.pft.sandbox;
 
+/**
+ * Created by mocius on 2017-03-19.
+ */
 public class MyFirstProgram {
+    public static void main(String[]args){
+        System.out.println("Hello, world");
+        System.out.println("Hello, Marcin");
 
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+        Point p = new Point(3,3,5,6);
+        p.x1 =3;
+        p.x2 = 3;
+        p.y1 = 5;
+        p.y2 = 6;
+
+
     }
+
+
 
 }
