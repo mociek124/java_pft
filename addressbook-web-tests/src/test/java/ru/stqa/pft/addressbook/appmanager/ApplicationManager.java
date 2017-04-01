7fc1b245bb2b87446349832a50da9f15bf19ce76
mociek124 <mociek124@gmail.com>
2017-04-01 15:53:58 +0200

    zadanie 7

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -2,23 +2,41 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by mocius on 2017-03-26.
  */
 public class ApplicationManager {
-  FirefoxDriver wd;
+  WebDriver wd;
 
   private SessionHelper sessionHelper;
   private  NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private String browser;
 
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
 
   public void init() {
-    wd = new FirefoxDriver();
+    if (Objects.equals(browser, BrowserType.FIREFOX)){
+      wd = new FirefoxDriver();
+    }else if (Objects.equals(browser, BrowserType.CHROME)){
+      wd = new ChromeDriver();
+    }else if (Objects.equals(browser, BrowserType.IE)) {
+      wd = new InternetExplorerDriver();
+    }
+
+
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
