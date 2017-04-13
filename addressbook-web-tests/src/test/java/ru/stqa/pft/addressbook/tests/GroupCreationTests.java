6d72b61e599f75e7332f72bf07f459395599906b
mociek124 <mociek124@gmail.com>
2017-04-13 17:30:28 +0200

    wartosc lambda

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -22,14 +23,7 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max =0;
-    for (GroupData g : after){
-      if (g.getId() > max){
-       max = g.getId();
-      }
-    }
-
-    group.setId(max);
+    group.setId(after.stream().max((o1 ,o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after));
   }
