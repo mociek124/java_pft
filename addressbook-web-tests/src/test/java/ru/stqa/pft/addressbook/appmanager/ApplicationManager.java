a5b91349ab1bbd4c91daa502145e2699e7ca4da4
mociek124 <mociek124@gmail.com>
2017-04-29 11:49:48 +0200

    zadanie 10 szostego dzialu

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -9,13 +9,19 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by mocius on 2017-03-26.
  */
 public class ApplicationManager {
+  private final Properties properties;
   WebDriver wd;
 
   private SessionHelper sessionHelper;
@@ -26,12 +32,19 @@ public class ApplicationManager {
 
 
   public ApplicationManager(String browser) {
+
     this.browser = browser;
+    properties = new Properties();
   }
 
 
-  public void init() {
-    if (Objects.equals(browser, BrowserType.FIREFOX)){
+
+
+  public void init() throws IOException {
+    String target = System.getProperty("target", "local");
+    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+    if (browser.equals( BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
     }else if (Objects.equals(browser, BrowserType.CHROME)){
       wd = new ChromeDriver();
@@ -39,13 +52,13 @@ public class ApplicationManager {
       wd = new InternetExplorerDriver();
     }
 
-
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook");
+    wd.get(properties.getProperty("web.baseUrl"));
     groupHelper = new GroupHelper(wd);
+    contactHelper = new ContactHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
-    sessionHelper.login("admin");
+    sessionHelper.login(properties.getProperty("web.adminLogin"));
   }
 
 
