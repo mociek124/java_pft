2ccda361d8d609b194edf3317942b8281c0db124
mociek124 <mociek124@gmail.com>
2017-05-01 18:55:26 +0200

    zadanie 4 siodmego dzialu

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -29,6 +29,7 @@ public class ApplicationManager {
   private GroupHelper groupHelper;
   private ContactHelper contactHelper;
   private String browser;
+  private DbHelper dbHelper;
 
 
   public ApplicationManager(String browser) {
@@ -43,6 +44,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
+    dbHelper = new DbHelper();
 
     if (browser.equals( BrowserType.FIREFOX)){
       wd = new FirefoxDriver();
@@ -59,6 +62,8 @@ public class ApplicationManager {
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login(properties.getProperty("web.adminLogin"));
+
+
   }
 
 
@@ -76,6 +81,10 @@ public class ApplicationManager {
   }
 
   public ContactHelper contactHelper(){ return contactHelper; }
+
+  public DbHelper db(){
+    return dbHelper;
+  }
 
 
 
